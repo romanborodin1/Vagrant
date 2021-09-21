831daff7aa0a7e8933e34629c160ef1b559b3366
Roman Borodin <borodinrs1@gmail.com>
2021-09-21 08:46:03 +0300

    Vagrantfile.1node, script.sh - upgrade shell provisioner feature

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+yum update -y
+
 # Delete default route in Vagrant VM-box like: "default via 10.0.2.2 dev eth0 proto dhcp metric 100"
 eval `ip route show default  | awk '{ if ($5 =="eth0" && $2 != "0.0.0.0") print "ip route del default dev " $5; }'`
 
